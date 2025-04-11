23082c2bf504fa78d1143c72c335aafb1436c62c
Iago Moreira <iagomoreira2353@gmail.com>
2025-04-11 18:19:24 -0300

    setup: Gerando estrutura inicial do projeto spring.

---
+++ src/main/java/com/ghostPipe/backend/config/EnvConfig.java
@@ -0,0 +1,16 @@
+package com.ghostPipe.backend.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EnvConfig {
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.configure()
+                .directory(System.getProperty("user.dir"))
+                .ignoreIfMissing()
+                .load();
+    }
+}
