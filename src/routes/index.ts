b1d9246d981f4e4109df44c54eb97a6ef8c19a1e
Gaaaybe <gabenesus@gmail.com>
2025-05-02 01:46:41 -0300

    fix: Resolve problemas de docker e Prisma

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,10 @@
 import { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+prisma.$connect().catch((e: any) => {
+  console.error("Error connecting to the database:", e);});
 
 const routes = (app: Express) => {
   app
