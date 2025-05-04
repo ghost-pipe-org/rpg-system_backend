f032d0c678e863fa1d7281aca983899b6aa93502
Gaaaybe <gabenesus@gmail.com>
2025-05-04 01:31:19 -0300

    fix: Rename field "password" to "password_hash" and add prisma client

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,15 @@
-import { Express, Request, Response } from "express";
+import type { Express, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-prisma.$connect().catch((e: any) => {
-  console.error("Error connecting to the database:", e);});
+// prisma.$connect().catch((e: any) => {
+//   console.error("Error connecting to the database:", e);});
 
 const routes = (app: Express) => {
-  app
-    .route("/")
-    .get((req: Request, res: Response) => res.status(200).send("API Node.js"));
+	app
+		.route("/")
+		.get((req: Request, res: Response) => res.status(200).send("API Node.js"));
 };
 
 export default routes;
