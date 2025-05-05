4dc2293d8940fcc3b940e751f4d739068cbd3ed1
cls2311 <caiolopes858@gmail.com>
2025-05-05 20:34:13 -0300

    teste

--- src/main/java/com/ghostPipe/backend/services/PlayerService.java
+++ src/main/java/com/ghostPipe/backend/services/PlayerService.java
@@ -1,5 +1,33 @@
 package com.ghostPipe.backend.services;
 
-public class PlayerService {
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.ghostPipe.backend.dto.PlayerResponseDTO;
+import com.ghostPipe.backend.repositories.PlayerRepository;
+
+@Service
+public class PlayerService {
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public List<PlayerResponseDTO> getAll() {
+        return playerRepository.findAll().stream()
+            .map(player -> new PlayerResponseDTO(
+                player.getId(),
+                player.getName(),
+                player.getEnrollment(),
+                player.getPhoneNumber(),
+                player.getEmail(),
+                player.getCreatedAt(),
+                player.getUpdatedAt()
+            ))
+            .collect(Collectors.toList());
+    }
 }
