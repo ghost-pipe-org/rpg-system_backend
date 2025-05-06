613a08cdfe7d0a7f0175330f53bb475459d92b79
Gabriel Menezes <130420155+Gaaaybe@users.noreply.github.com>
2025-05-05 23:40:17 -0300

    feat: add registration validation middleware and refactor register controller to use it

--- src/controllers/users/registerController.ts
+++ src/controllers/users/registerController.ts
@@ -4,23 +4,8 @@ import z from "zod";
 import type { Request, Response } from "express";
 
 export async function registerController(req: Request, res: Response) {
-	const registerSchema = z.object({
-		name: z.string().min(1, { message: "Name is required" }),
-		email: z.string().email({ message: "Invalid email address" }),
-		password: z
-			.string()
-			.min(6, { message: "Password must be at least 6 characters long" })
-			.regex(/[A-Z]/, {
-				message: "Password must contain at least one uppercase letter",
-			})
-			.regex(/[a-z]/, {
-				message: "Password must contain at least one lowercase letter",
-			})
-			.regex(/[0-9]/, { message: "Password must contain at least one number" }),
-		role: z.enum(["admin", "user"]).default("user"),
-	});
 
-	const { name, email, password, role } = registerSchema.parse(req.body);
+	const {name, email, password, role} = req.body
 
 	try {
 		const registerService = makeRegisterService();
