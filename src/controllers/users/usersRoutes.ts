9c87f1e963fc35bb6435acca716f1c150de9d20b
Gaaaybe <gabenesus@gmail.com>
2025-05-05 21:10:46 -0300

    feat: Implement authenticate controller and add authentication route

--- src/controllers/users/usersRoutes.ts
+++ src/controllers/users/usersRoutes.ts
@@ -1,6 +1,8 @@
 import type { Express } from 'express';
 import { registerController } from './registerController';
+import { authenticateController } from './authenticateController';
 
 export async function usersRoutes(app :Express){
     app.post("/users", registerController);
+    app.post("/users/authenticate", authenticateController);
 }
