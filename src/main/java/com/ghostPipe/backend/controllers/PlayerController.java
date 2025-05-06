cc4050995e7f8974bda6eae75842513eaff43a8e
cls2311 <caiolopes858@gmail.com>
2025-05-06 10:31:11 -0300

    Feat: adding POST route to Controller

--- src/main/java/com/ghostPipe/backend/controllers/PlayerController.java
+++ src/main/java/com/ghostPipe/backend/controllers/PlayerController.java
@@ -1,5 +1,6 @@
 package com.ghostPipe.backend.controllers;
 
+import com.ghostPipe.backend.dto.PlayerRequestDTO;
 import com.ghostPipe.backend.dto.PlayerResponseDTO;
 import com.ghostPipe.backend.services.PlayerService;
 
@@ -10,6 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/players")
@@ -26,4 +30,10 @@ public class PlayerController {
         List<PlayerResponseDTO> players = playerService.getAll(); 
         return ResponseEntity.ok(players);
     }
+@PostMapping
+public ResponseEntity<PlayerResponseDTO> createPlayer(@RequestBody PlayerRequestDTO request){
+    PlayerResponseDTO createdPlayer = playerService.createPlayer(request);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdPlayer);
+}
+
 }
--- src/main/java/com/ghostPipe/backend/services/PlayerService.java
+++ src/main/java/com/ghostPipe/backend/services/PlayerService.java
@@ -30,4 +30,5 @@ public class PlayerService {
             ))
             .collect(Collectors.toList());
     }
+    
 }
