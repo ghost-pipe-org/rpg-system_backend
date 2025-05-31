87c40b8a9d3f52c62877b7c14651a746f57a30f7
Cls2311 <caiolopes858@gmail.com>
2025-05-31 19:37:55 -0300

    refactor: changing PlayerRequestDTO responses to english

--- src/main/java/com/ghostPipe/backend/dto/PlayerRequestDTO.java
+++ src/main/java/com/ghostPipe/backend/dto/PlayerRequestDTO.java
@@ -1,34 +1,65 @@
 package com.ghostPipe.backend.dto;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Builder;
 
 @Getter
 @Setter
-@Builder
 public class PlayerRequestDTO {
-
-    @NotBlank(message = "Este campo é obrigatório")
-    @Size(max = 100, message = "Você excedeu o número máximo de caracteres permitidos")
+    @NotBlank(message = "This field is required")
+    @Size(max = 100, message = "You have exceeded the maximum allowed number of characters")
     private String name;
 
-    @Size(max = 9, message = "Matrícula inválida")
+    @Size(max = 9, message = "Invalid enrollment number")
     private String enrollment;
 
-    @Size(max = 20, message = "Número de telefone inválido")
-    private String phoneNumber;
+    @Size(max = 20, message = "Invalid phone number")
+    private String phone;
 
-    @NotBlank(message = "Este campo é obrigatório")
-    @Size(min = 6, max = 255, message = "A senha deve ter pelo menos seis caracteres")
-    private String password;
+    @NotBlank(message = "This field is required")
+    @Size(min = 6, max = 255, message = "Password must be at least six characters long")
+    private String encryptedpassword;
 
-    @NotBlank(message = "Este campo é obrigatório")
-    @Email(message = "O email deve ser válido")
-    @Size(max = 255, message = "Número de caracteres excedido")
+    @NotBlank(message = "This field is required")
+    @Email(message = "Email must be valid")
+    @Size(max = 255, message = "Maximum number of characters exceeded")
     private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(String enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEncryptedpassword() {
+        return encryptedpassword;
+    }
+
+    public void setEncryptedpassword(String encryptedpassword) {
+        this.encryptedpassword = encryptedpassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
