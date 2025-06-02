7c7d50b15fd584aa32a63e772b2fc80e7a706e3b
Cls2311 <caiolopes858@gmail.com>
2025-06-01 21:48:44 -0300

    feat: adds new message to error DTO

--- src/main/java/com/ghostPipe/backend/dto/ErrorResponseDTO.java
+++ src/main/java/com/ghostPipe/backend/dto/ErrorResponseDTO.java
@@ -1,14 +1,22 @@
 package com.ghostPipe.backend.dto;
 
+import org.springframework.http.HttpStatus;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import java.time.LocalDateTime;
 
 @Data
 @Builder
 @AllArgsConstructor
 public class ErrorResponseDTO {
     private String message;
-    private LocalDateTime timestamp = LocalDateTime.now();
+    private LocalDateTime timestamp;
+    private HttpStatus status;
+
+    public ErrorResponseDTO(String message) {
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+        this.status = HttpStatus.INTERNAL_SERVER_ERROR;
+    }
 }
