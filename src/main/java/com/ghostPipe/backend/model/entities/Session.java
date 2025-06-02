6f79744e886388b727c6413bbd4bb369b9892382
Cls2311 <caiolopes858@gmail.com>
2025-06-02 20:16:21 -0300

    feat: adds Session entity

---
+++ src/main/java/com/ghostPipe/backend/model/entities/Session.java
@@ -0,0 +1,59 @@
+package com.ghostPipe.backend.model.entities;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Converter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table (name = "session")
+public class Session {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull
+    private int min_players;
+
+    @NotNull
+    private int max_players;
+
+    @NotNull
+    private String session_title;
+
+    @NotNull
+    private String session_description;
+
+    private String session_requirements;
+
+    private SolicitationStatus solicitation_status;
+
+    @NotNull
+    private String session_system;
+
+    @NotNull
+    private String location;
+
+    @NotNull
+    @Column(name = "possible_dates", lenght = 100, nullable = false)
+    @Convert(converter = PossibleDatesConverter.class)
+    private List<LocalDate> possible_dates;
+
+    private String approved_date;
+
+    private String cancel_event;
+
+    private SessionPeriod session_period;
+
+    private LocalDateTime created_at;
+}
