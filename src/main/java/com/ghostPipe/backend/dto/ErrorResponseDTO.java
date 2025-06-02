0a88110d7c39903b91128ea761c44ef5436db63a
Cls2311 <caiolopes858@gmail.com>
2025-06-01 21:43:32 -0300

    fix: adding constructors  to ErrorResponseDTO

--- src/main/java/com/ghostPipe/backend/dto/ErrorResponseDTO.java
+++ src/main/java/com/ghostPipe/backend/dto/ErrorResponseDTO.java
@@ -1,9 +1,13 @@
 package com.ghostPipe.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class ErrorResponseDTO {
     private String message;
     private LocalDateTime timestamp = LocalDateTime.now();
