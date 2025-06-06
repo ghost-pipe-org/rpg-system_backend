ffa1b6792b9cee73486eb1b82479df9ca9134593
Gabriel Menezes <130420155+Gaaaybe@users.noreply.github.com>
2025-06-06 07:16:47 -0300

    feat: adicionar suporte a CORS e atualizar dependências para @types/cors

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import { env } from "@/env";
 import app from "./src/app";
 import { prisma } from "./src/lib/prisma";
 import express from "express";
+import cors from "cors";
 
 const PORT = env.PORT || 3001;
 
@@ -12,6 +13,7 @@ const startServer = async () => {
         app.listen(PORT, () => {
             console.log(`Server is running on http://localhost:${PORT}`);
         });
+        app.use(cors());
         app.use(express.json());
     } catch (error) {
         console.error('Failed to connect to the database:', error);
