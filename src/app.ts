07930e326cd285154129d3438588097fb3a5c854
Gaaaybe <gabenesus@gmail.com>
2025-06-27 18:32:37 -0300

    feat: atualizar configuração de CORS para permitir múltiplas origens e métodos

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,13 @@ const app = express();
 
 // Adicione os middlewares globais aqui
 app.use(cors({
-  origin: "https://rpg-system-frontend.vercel.app",
-  credentials: true
+  origin: [
+    "https://rpg-system-frontend.vercel.app",
+    "http://localhost:5173"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 
